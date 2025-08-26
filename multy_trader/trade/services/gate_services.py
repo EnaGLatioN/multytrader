1d84a6a7e2e7fb68e565e24fdc3322ce1505cadc
v.kondratov <ubwork@UBwork.myguest.virtualbox.org>
2025-08-26 22:55:55 +0500

    add script

--- multy_trader/trade/services/gate_services.py
+++ multy_trader/trade/services/gate_services.py
@@ -1,4 +1,5 @@
 from gate_api import ApiClient, Configuration, FuturesApi
+from multy_trader.trade.models import TradeType
 import time
 from multy_trader.settings import GATE_HOST
 
@@ -9,7 +10,7 @@ def gate_buy_futures_contract(entry,order):
     """
 
     contract=entry.wallet_pair.slug # Например, BTC_USDT для бессрочного контракта
-    amount=''#entry.count if order.trade_type == TradeType.LONG else -entry.count # Количество BTC
+    amount=entry.profit if order.trade_type == TradeType.LONG else -entry.count # Количество BTC превращать из суммы в кол-во
     price=order.entry_course  # None для рыночного ордера
     take_profit=entry.exit_course # Курс выхода
     order_type="market"  # "market" или "limit"
